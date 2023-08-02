1219b3a67b9128482d57c4fc9e7c9a2af4f7ad8b
LaoSparrow <laosparrow@outlook.com>
2023-08-03 01:48:22 +0800

    fix: chatbox malfunctioning

--- CJKFontFixer.TAPlugin/CJKFontFixer.cs
+++ CJKFontFixer.TAPlugin/CJKFontFixer.cs
@@ -16,8 +16,8 @@ public class CJKFontFixer : Plugin
     #region Font File Names
 
     public static string ChineseFontName => $"{ClientLoader.AssetPath}/SourceHanSansSC-VF.ttf";
-    public static string JapaneseFontName => $"{ClientLoader.AssetPath}/SourceHanSans-VF.ttf";
-    public static string KoreanFontName => $"{ClientLoader.AssetPath}/SourceHanSansK-VF.ttf";
+    // public static string JapaneseFontName => $"{ClientLoader.AssetPath}/SourceHanSans-VF.ttf";
+    // public static string KoreanFontName => $"{ClientLoader.AssetPath}/SourceHanSansK-VF.ttf";
 
     #endregion
 
@@ -37,28 +37,34 @@ public class CJKFontFixer : Plugin
 
     public override void Load()
     {
-        Utils.ExtractFontFiles();
-        
-        TerrariaFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
-            .GetField("TerrariaFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
-        MonospaceFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
-            .GetField("MonospaceFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
-        ClientLoader.MainRenderer?.EnqueuePreDrawAction(() =>
-        {
-            TerrariaFonts.Clear();
-            MonospaceFonts.Clear();
-            
-            ImGui.GetIO().Fonts.AddFontDefault();
-            ReloadFonts();
-            ClientLoader.MainRenderer?.RebuildFontAtlas();
-        });
+
     }
 
-    public override void Unload()
+    private int counter;
+    public override void Update()
     {
+        if (counter <= 30)
+            counter++;
+        if (counter == 30)
+        {
+            Utils.ExtractFontFiles();
         
+            TerrariaFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
+                .GetField("TerrariaFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
+            MonospaceFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
+                .GetField("MonospaceFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
+            ClientLoader.MainRenderer?.EnqueuePreDrawAction(() =>
+            {
+                TerrariaFonts.Clear();
+                MonospaceFonts.Clear();
+            
+                ImGui.GetIO().Fonts.AddFontDefault();
+                ReloadFonts();
+                ClientLoader.MainRenderer?.RebuildFontAtlas();
+            });
+        }
     }
-    
+
     #endregion
 
     #region Font Reloading
@@ -75,12 +81,12 @@ public class CJKFontFixer : Plugin
     public static void ReloadTerrariaFont(float size, bool withoutSymbols = false)
     {
         TerrariaFonts!.Add(size, ClientAssets.LoadFont(ClientAssets.TerrariaFontName, size, 0x0020, 0x007F));
-        ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x0080, 0x00FF, 0x0400, 0x04FF, 0x2320, 0x2330, 0x2000, 0x2020);
+        // ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x0080, 0x00FF, 0x0400, 0x04FF, 0x2320, 0x2330, 0x2000, 0x2020);
         Utils.LoadFont(ChineseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.ChineseFull);
         // It seems that Simplified Chinese Font contains Japanese?
         // If you found any issue on displaying Japanese, just uncomment this line as well as line 96
         // Utils.LoadFont(JapaneseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Japanese);
-        Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
+        // Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
 
         if (!withoutSymbols)
         {
@@ -91,10 +97,10 @@ public class CJKFontFixer : Plugin
     public static void ReloadMonospaceFont(float size, bool withoutSymbols = false)
     {
         MonospaceFonts!.Add(size, ClientAssets.LoadFont(ClientAssets.MonoFontName, size, 0x0020, 0x00FF, 0x0400, 0x04FF, 0x2020, 0x22FF));
-        ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x2320, 0x2330, 0x2000, 0x2020);
+        // ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x2320, 0x2330, 0x2000, 0x2020);
         Utils.LoadFont(ChineseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.ChineseFull);
         // Utils.LoadFont(JapaneseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Japanese);
-        Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
+        // Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
 
         if (!withoutSymbols)
         {
