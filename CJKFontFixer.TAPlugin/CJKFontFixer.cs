324da217a68ff5ede70c168427c3c62c476f6d08
LaoSparrow <laosparrow@outlook.com>
2023-08-03 00:18:14 +0800

    chore: initial commit

---
+++ CJKFontFixer.TAPlugin/CJKFontFixer.cs
@@ -0,0 +1,106 @@
+﻿using System.Numerics;
+using System.Reflection;
+using ImGuiNET;
+using TerraAngel;
+using TerraAngel.Assets;
+using TerraAngel.Plugin;
+
+namespace CJKFontFixer.TAPlugin;
+
+// ReSharper disable once InconsistentNaming
+// ReSharper disable once UnusedType.Global
+public class CJKFontFixer : Plugin
+{
+    public override string Name => "CJK Font Fixer";
+
+    #region Font File Names
+
+    public static string ChineseFontName => $"{ClientLoader.AssetPath}/SourceHanSansSC-VF.ttf";
+    public static string JapaneseFontName => $"{ClientLoader.AssetPath}/SourceHanSans-VF.ttf";
+    public static string KoreanFontName => $"{ClientLoader.AssetPath}/SourceHanSansK-VF.ttf";
+
+    #endregion
+
+    #region Extracted ClientAssets Fields
+
+    private static Dictionary<float, ImFontPtr>? TerrariaFonts;
+    private static Dictionary<float, ImFontPtr>? MonospaceFonts;
+
+    #endregion
+
+    #region Constructor / Load / Unload
+
+    public CJKFontFixer(string path) : base(path)
+    {
+
+    }
+
+    public override void Load()
+    {
+        Utils.ExtractFontFiles();
+        
+        TerrariaFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
+            .GetField("TerrariaFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
+        MonospaceFonts = (Dictionary<float, ImFontPtr>)typeof(ClientAssets)
+            .GetField("MonospaceFonts", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
+        ClientLoader.MainRenderer?.EnqueuePreDrawAction(() =>
+        {
+            TerrariaFonts.Clear();
+            MonospaceFonts.Clear();
+            
+            ImGui.GetIO().Fonts.AddFontDefault();
+            ReloadFonts();
+            ClientLoader.MainRenderer?.RebuildFontAtlas();
+        });
+    }
+
+    public override void Unload()
+    {
+        
+    }
+    
+    #endregion
+
+    #region Font Reloading
+
+    public static void ReloadFonts()
+    {
+        foreach (var size in ClientAssets.DefaultSizes)
+        {
+            ReloadMonospaceFont(size);
+            ReloadTerrariaFont(size);
+        }
+    }
+
+    public static void ReloadTerrariaFont(float size, bool withoutSymbols = false)
+    {
+        TerrariaFonts!.Add(size, ClientAssets.LoadFont(ClientAssets.TerrariaFontName, size, 0x0020, 0x007F));
+        ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x0080, 0x00FF, 0x0400, 0x04FF, 0x2320, 0x2330, 0x2000, 0x2020);
+        Utils.LoadFont(ChineseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.ChineseFull);
+        // It seems that Simplified Chinese Font contains Japanese?
+        // If you found any issue on displaying Japanese, just uncomment this line as well as line 96
+        // Utils.LoadFont(JapaneseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Japanese);
+        Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
+
+        if (!withoutSymbols)
+        {
+            ClientAssets.LoadFont(ClientAssets.IconFontName, size, true, new Vector2(0f, 4f), Icon.IconMin, Icon.IconMax);
+        }
+    }
+    
+    public static void ReloadMonospaceFont(float size, bool withoutSymbols = false)
+    {
+        MonospaceFonts!.Add(size, ClientAssets.LoadFont(ClientAssets.MonoFontName, size, 0x0020, 0x00FF, 0x0400, 0x04FF, 0x2020, 0x22FF));
+        ClientAssets.LoadFont(ClientAssets.FallbackFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, 0x2320, 0x2330, 0x2000, 0x2020);
+        Utils.LoadFont(ChineseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.ChineseFull);
+        // Utils.LoadFont(JapaneseFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Japanese);
+        Utils.LoadFont(KoreanFontName, size, true, Vector2.Zero, Vector2.Zero, 0f, float.MaxValue, 2f, Utils.GlyphRanges.Korean);
+
+        if (!withoutSymbols)
+        {
+            ClientAssets.LoadFont(ClientAssets.IconFontName, size, true, new Vector2(0f, 4f), Icon.IconMin, Icon.IconMax);
+        }
+    }
+    
+    #endregion
+}
---
+++ CJKFontFixer.TAPlugin/Utils.cs
@@ -0,0 +1,68 @@
+﻿using System.Numerics;
+using System.Reflection;
+using ImGuiNET;
+using TerraAngel;
+
+namespace CJKFontFixer.TAPlugin;
+
+internal static class Utils
+{
+    internal static void ExtractFontFiles()
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        var fontFiles = asm
+            .GetManifestResourceNames()
+            .Where(x => x.EndsWith(".ttf"))
+            .Select(x => new
+            {
+                FileName = x,
+                Stream = asm.GetManifestResourceStream(x)
+            });
+        foreach (var x in fontFiles)
+        {
+            var fileName = x.FileName.Substring("CJKFontFixer.TAPlugin.Resources.".Length,
+                x.FileName.Length - "CJKFontFixer.TAPlugin.Resources.".Length);
+            var path = $"{ClientLoader.AssetPath}/{fileName}";
+            if (File.Exists(path))
+                continue;
+            
+            using var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            x.Stream!.CopyTo(fs);
+        }
+    }
+
+    internal static class GlyphRanges
+    {
+        public static IntPtr ChineseFull => ImGui.GetIO().Fonts.GetGlyphRangesChineseFull();
+        public static IntPtr Japanese => ImGui.GetIO().Fonts.GetGlyphRangesJapanese();
+        public static IntPtr Korean => ImGui.GetIO().Fonts.GetGlyphRangesKorean();
+    }
+    
+    internal static unsafe ImFontPtr LoadFont(string path, float size, bool merge, Vector2 glyphOffset, Vector2 glyphExtraSpacing, float minAdvanceX, float maxAdvanceX, float rasterizerMultiply, IntPtr glyphRanges)
+    {
+        ImFontConfigPtr config = ImGuiNative.ImFontConfig_ImFontConfig();
+        ImFontPtr font = LoadFont(path, size, merge, glyphOffset, glyphExtraSpacing, minAdvanceX, maxAdvanceX, config.OversampleH, config.OversampleV, config.PixelSnapH, rasterizerMultiply, glyphRanges);
+        config.Destroy();
+        return font;
+    }
+    
+    internal static unsafe ImFontPtr LoadFont(string path, float size, bool merge, Vector2 glyphOffset, Vector2 glyphExtraSpacing, float minAdvanceX, float maxAdvanceX, int overSampleH, int overSampleV, bool pixelSnapH, float rasterizerMultiply, IntPtr glyphRanges)
+    {
+        ImGuiIOPtr io = ImGui.GetIO();
+        ImFontConfigPtr config = ImGuiNative.ImFontConfig_ImFontConfig();
+
+        config.MergeMode = merge;
+        config.GlyphOffset = glyphOffset;
+        config.GlyphExtraSpacing = glyphExtraSpacing;
+        config.GlyphMinAdvanceX = minAdvanceX;
+        config.GlyphMaxAdvanceX = maxAdvanceX;
+        config.OversampleH = overSampleH;
+        config.OversampleV = overSampleV;
+        config.PixelSnapH = pixelSnapH;
+        config.RasterizerMultiply = rasterizerMultiply;
+        
+        ImFontPtr font = io.Fonts.AddFontFromFileTTF(path, size, config, glyphRanges);
+        config.Destroy();
+        return font;
+    }
+}
